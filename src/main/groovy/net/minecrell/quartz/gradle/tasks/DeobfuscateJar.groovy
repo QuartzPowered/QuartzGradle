1e8a4b483451b81fe5c469f2eddb8f2f7ea07ca9
Minecrell <dev@minecrell.net>
2015-03-24 13:21:43 +0100

    Add missing license headers

--- src/main/groovy/net/minecrell/quartz/gradle/tasks/DeobfuscateJar.groovy
+++ src/main/groovy/net/minecrell/quartz/gradle/tasks/DeobfuscateJar.groovy
@@ -1,3 +1,25 @@
+/*
+ * QuartzGradle
+ * Copyright (c) 2015, Minecrell <https://github.com/Minecrell>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package net.minecrell.quartz.gradle.tasks
 
 import net.minecrell.quartz.mappings.MappedClass
@@ -5,7 +27,6 @@ import net.minecrell.quartz.mappings.loader.Mappings
 import net.minecrell.quartz.mappings.transformer.MappingsTransformer
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.FileCollection
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.InputFiles
 import org.gradle.api.tasks.OutputFile
